644b32713eb6f8e8cc3ff9ee6455bddd3d742cd9
Yury Sauko <yury.savko@gmail.com>
2023-04-13 13:18:03 +0300

    The slider in the third section, CAROUSEL, is completed

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carousel__wrapper').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left-arrow.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right-arrow.svg"></button>'
+      });
+  });
