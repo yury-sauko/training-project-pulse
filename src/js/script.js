611c21f2ec2337e976d42c0603aaaf1d36a8585c
Yury Sauko <yury.savko@gmail.com>
2023-04-17 11:02:03 +0300

    Changed the slider to Tiny Slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,16 @@
-$(document).ready(function(){
-    $('.carousel__wrapper').slick({
-        speed: 1200,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left-arrow.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right-arrow.svg"></button>'
-      });
-  });
+const slider = tns({
+    container: '.carousel__wrapper',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+});
