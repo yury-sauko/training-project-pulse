eaa068e776ec4587a4c5265f0e9410920b02d82f
Yury Sauko <yury.savko@gmail.com>
2023-04-26 23:42:44 +0300

    Modal windows is complited, without validation

--- src/js/script.js
+++ src/js/script.js
@@ -17,6 +17,8 @@ document.querySelector('.next').addEventListener('click', function () {
 
 $(document).ready(function(){
 
+    // Tabs
+
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
         .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
@@ -35,5 +37,22 @@ $(document).ready(function(){
 
     toggleCatalogItem ('.catalog-item__link');
     toggleCatalogItem ('.catalog-item__list-back');
+
+    // Modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+
+    $('.button_catalog-item').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
 
 });
